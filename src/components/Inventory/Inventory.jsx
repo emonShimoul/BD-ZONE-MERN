d78b03eac1155fdae4b4f26578aec00f7d0256f6
emonShimoul <emonshimoul@gmail.com>
2023-05-07 15:55:03 +0600

    use react router

---
+++ src/components/Inventory/Inventory.jsx
@@ -0,0 +1,9 @@
+const Inventory = () => {
+  return (
+    <div>
+      <h2>Inventory Page</h2>
+    </div>
+  );
+};
+
+export default Inventory;
---
+++ src/components/Orders/Orders.jsx
@@ -0,0 +1,10 @@
+const Orders = () => {
+  return (
+    <div>
+      <h2>Orders Page</h2>
+    </div>
+  );
+};
+
+export default Orders;
+<h2>Orders Page</h2>;
