e1d8ae9376a44d3a437e0fc8fb9cd8bf1f1f349f
emonShimoul <emonshimoul@gmail.com>
2023-05-09 01:16:49 +0600

    added Order and ReviewItem components and design them

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -1,13 +1,17 @@
 import { useLoaderData } from "react-router-dom";
 import Cart from "../Cart/Cart";
+import ReviewItem from "../ReviewItem/ReviewItem";
+import "./Orders.css";
 
 const Orders = () => {
   const cart = useLoaderData();
   console.log(cart);
   return (
     <div className="shop-container">
-      <div className="products-container">
-        <h2>Orders Page: {cart.length}</h2>
+      <div className="review-container">
+        {cart.map((product) => (
+          <ReviewItem key={product.id} product={product}></ReviewItem>
+        ))}
       </div>
       <div className="cart-container">
         <Cart cart={cart} />
