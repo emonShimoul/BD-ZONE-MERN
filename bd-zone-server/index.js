34a7ecbcb74a6076a24262248d02853947b319b8
emonShimoul <emonshimoul@gmail.com>
2023-06-07 12:04:39 +0600

    use post to load some products using keys

--- bd-zone-server/index.js
+++ bd-zone-server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,7 +10,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pabg0.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -29,7 +29,7 @@ async function run() {
     const productCollection = client.db("bdZoneDB").collection("products");
 
     app.get("/products", async (req, res) => {
-      console.log(req.query);
+      // console.log(req.query);
       const page = parseInt(req.query.page) || 0;
       const limit = parseInt(req.query.limit) || 10;
       const skip = page * limit;
@@ -45,6 +45,16 @@ async function run() {
     app.get("/totalProducts", async (req, res) => {
       const result = await productCollection.estimatedDocumentCount();
       res.send({ totalProducts: result });
+    });
+
+    app.post("/productsByIds", async (req, res) => {
+      const ids = req.body;
+      // const objectIds = ids.map((id) => new ObjectId(id));
+      // console.log(objectIds);
+      const query = { _id: { $in: ids } };
+      console.log(ids);
+      const result = await productCollection.find(query).toArray();
+      res.send(result);
     });
 
     // Send a ping to confirm a successful connection
